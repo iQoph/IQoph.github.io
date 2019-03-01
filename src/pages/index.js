d93d98b3a15abe575467f1ed00676e8a3d43601d
iqoph <hmn0616@gmail.com>
2019-03-02 04:35:01 +0900

    アイコス　トップページの文章変更した

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h2>アイコスのタグ</h2>
+    <h2>Welcome to my world</h2>
+    <p>I'm one of the drunkards in Japan.</p>
+    <p>だそうです。</p>
   </Layout>
 )
 
